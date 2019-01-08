7f0fd098389ce1576fc85b0ab09eb5b534235d70
NoelToy <noel.toy@flutura.com>
2019-01-08 17:44:04 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private serverUrl = 'http://localhost:8080/websocket'
+  private title = 'WebSockets chat';
+  private stompClient;
+  someText:string;
+  messageStore=[];
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/test/queue", (message) => {
+        if(message.body) {
+          that.messageStore.push(JSON.parse(message.body).content);
+        }
+      });
+    });
+  }
+  sendMessage(){
+    this.stompClient.send("/app/chat" , {}, JSON.stringify({'name':this.someText}));
+    this.someText="";
+  }
+
+  disconnect(){
+    this.stompClient.disconnect();
+  }
+  
+
+
+
 }
